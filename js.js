80c92f3cded634ee672cce0fc444020e4c4d3a6b
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-03-31 15:57:07 +0500

    2q

---
+++ js.js
@@ -0,0 +1,17 @@
+// Ek classroom mein 5 chairs hain line se. 3 students ko un par bithana hai. Kitne tareeqon se wo baith sakte hain? 60
+/* 
+Permutation nikalna hy 
+N = 5
+R = 3
+ 
+ab hoga ea k hm 5 ka Factorial nikalen gy mtlb 5*4*3*2*1=120 
+phr N ko R minus kren gy 5 - 3 = 2
+N ko taqsim kren gy n-r ki Factorial sy mtlb 120/2=60 
+
+*/
+
+// Ek lafz hai "CODE". Is mein 4 letters hain. In 4 letters ko aage peeche karke tum kitne naye "words" bana sakte ho? (Yahan tum charo letters use kar rahe ho). 4*3*2*1=24
+// n=4
+//  r=4
+// Hint 
+// Jab n aur r barabar hon (n=r), toh formula n! ban jata hai.
